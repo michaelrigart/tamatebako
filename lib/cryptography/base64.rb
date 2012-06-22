8c82255cbf24f66ad6135c08125f44d5bcc37d6c
Michaël Rigart <michael@netronix.be>
2012-06-22 17:15:00 +0200

    remove tabs with spaces

--- lib/cryptography/base64.rb
+++ lib/cryptography/base64.rb
@@ -1,17 +1,17 @@
 module Tamatebako
-	module Cryptography
-		module Base64
-			module_function
-			require 'base64'
+  module Cryptography
+    module Base64
+      module_function
+      require 'base64'
 
 
-			def encode(string)
-				::Base64.encode64(string).gsub("\n", "")
-			end
+      def encode(string)
+        ::Base64.encode64(string).gsub("\n", "")
+      end
 
-			def decode(string)
-				::Base64.decode64(string)
-			end
-		end
-	end
+      def decode(string)
+        ::Base64.decode64(string)
+      end
+    end
+  end
 end
--- lib/cryptography/digest.rb
+++ lib/cryptography/digest.rb
@@ -1,30 +1,30 @@
 module Tamatebako
-	module Cryptography
-		module Digest
-			module_function
+  module Cryptography
+    module Digest
+      module_function
 
-			def hex(type, string)
-				case type
-					when "md5"
-						require "digest/md5"
-						::Digest::MD5.hexdigest(string)
-					when "sha1"
-						require "digest/sha1"
-						::Digest::SHA1.hexdigest(string)
-					when "sha256"
-						require "digest/sha2"
-						::Digest::SHA256.hexdigest(string)
-					when "sha384"
-						require "digest/sha2"
-						::Digest::SHA384.hexdigest(string)
-					when "sha512"
-						require "digest/sha2"
-						::Digest::SHA512.hexdigest(string)
-					when "rmd160"
-						require "digest/rmd160"
-						::Digest::RMD160.hexdigest(string)
-				end
-			end
-		end
-	end
+      def hex(type, string)
+        case type
+          when "md5"
+            require "digest/md5"
+            ::Digest::MD5.hexdigest(string)
+          when "sha1"
+            require "digest/sha1"
+            ::Digest::SHA1.hexdigest(string)
+          when "sha256"
+            require "digest/sha2"
+            ::Digest::SHA256.hexdigest(string)
+          when "sha384"
+            require "digest/sha2"
+            ::Digest::SHA384.hexdigest(string)
+          when "sha512"
+            require "digest/sha2"
+            ::Digest::SHA512.hexdigest(string)
+          when "rmd160"
+            require "digest/rmd160"
+            ::Digest::RMD160.hexdigest(string)
+        end
+      end
+    end
+  end
 end
